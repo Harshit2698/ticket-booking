b979523b744eae07f36d018af6fd07d5f9fe16e8
harshit2698 <harshitpalak97singh@gmail.com>
2025-06-25 09:49:20 +0530

    feat: add core entity classes, services, and local JSON data files

---
+++ app/src/main/java/ticket/booking/service/UserBookingService.java
@@ -0,0 +1,20 @@
+package ticket.booking.service;
+
+
+import ticket.booking.entities.User;
+
+import java.io.File;
+
+public class UserBookingService {
+
+    private User user;
+
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    public UserBookingService(User user1){
+
+        this.user = user1;
+        File users = new File(USERS_PATH);
+
+    }
+}
